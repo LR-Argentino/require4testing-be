639e756dbf8587c8caa62501262512942ab17cd3
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-06-01 12:57:53 +0200

    project setup

---
+++ src/main/java/org/blackbird/requirefortesting/RequirefortestingApplication.java
@@ -0,0 +1,13 @@
+package org.blackbird.requirefortesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RequirefortestingApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RequirefortestingApplication.class, args);
+  }
+
+}
