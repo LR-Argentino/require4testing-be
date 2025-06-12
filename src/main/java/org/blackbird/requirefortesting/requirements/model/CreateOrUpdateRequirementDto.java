411d3f0a8fbebd781cef48c43c2a52eb61499776
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-06-12 16:47:41 +0200

    feat: add updateRequirement endpoint and update application configuration

--- src/main/java/org/blackbird/requirefortesting/requirements/model/CreateOrUpdateRequirementDto.java
+++ src/main/java/org/blackbird/requirefortesting/requirements/model/CreateOrUpdateRequirementDto.java
@@ -2,4 +2,4 @@ package org.blackbird.requirefortesting.requirements.model;
 
 import org.blackbird.requirefortesting.shared.Priority;
 
-public record CreateRequirementDto(String title, String description, Priority priority) {}
+public record CreateOrUpdateRequirementDto(String title, String description, Priority priority) {}
