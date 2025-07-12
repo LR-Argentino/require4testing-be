40ff07c02b57d05f0b762ea578b8dc25dc5fcd58
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-07-12 20:19:11 +0200

    fix issue of lowercase

--- src/main/java/org/blackbird/requirefortesting/shared/Priority.java
+++ src/main/java/org/blackbird/requirefortesting/shared/Priority.java
@@ -1,7 +1,20 @@
 package org.blackbird.requirefortesting.shared;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Priority {
   HIGH,
   MEDIUM,
-  LOW,
+  LOW;
+
+  @JsonCreator
+  public static Priority fromString(String value) {
+    return Priority.valueOf(value.toUpperCase());
+  }
+
+  @JsonValue
+  public String toValue() {
+    return name();
+  }
 }
--- src/main/java/org/blackbird/requirefortesting/shared/Status.java
+++ src/main/java/org/blackbird/requirefortesting/shared/Status.java
@@ -1,7 +1,20 @@
 package org.blackbird.requirefortesting.shared;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonValue;
+
 public enum Status {
   OPEN,
   IN_PROGRESS,
-  CLOSED,
+  CLOSED;
+
+  @JsonCreator
+  public static Status fromString(String value) {
+    return Status.valueOf(value.toUpperCase());
+  }
+
+  @JsonValue
+  public String toValue() {
+    return name();
+  }
 }
