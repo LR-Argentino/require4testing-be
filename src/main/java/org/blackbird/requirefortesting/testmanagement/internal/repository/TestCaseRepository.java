d6471ace836f733febf49c9b3dd53118cfe29644
Quentino <30447209+LR-Argentino@users.noreply.github.com>
2025-07-13 12:24:42 +0200

    feat: add test case existence check before requirement deletion

--- src/main/java/org/blackbird/requirefortesting/testmanagement/internal/repository/TestCaseRepository.java
+++ src/main/java/org/blackbird/requirefortesting/testmanagement/internal/repository/TestCaseRepository.java
@@ -11,4 +11,6 @@ import org.springframework.stereotype.Repository;
 public interface TestCaseRepository extends JpaRepository<TestCase, Long> {
   @Query("SELECT t FROM TestCase t LEFT JOIN FETCH t.testRuns WHERE t.id = :id")
   Optional<TestCase> findByIdWithTestRuns(@Param("id") Long id);
+  
+  boolean existsByRequirementId(Long requirementId);
 }
